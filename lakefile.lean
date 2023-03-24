d18c4841c16229f9eea9beeda1ce034273773d63
Arthur Paulino <arthurleonardo.ap@gmail.com>
2023-03-24 10:43:07 -0300

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -6,7 +6,7 @@ package LightData
 lean_lib LightData
 
 require YatimaStdLib from git
-  "https://github.com/yatima-inc/YatimaStdLib.lean" @ "10f2b444390a41ede90ca5c038c6ff972014d433"
+  "https://github.com/lurk-lab/YatimaStdLib.lean" @ "10f2b444390a41ede90ca5c038c6ff972014d433"
 
 require LSpec from git
   "https://github.com/yatima-inc/lspec/" @ "88f7d23e56a061d32c7173cea5befa4b2c248b41"
