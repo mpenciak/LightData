5cc0bbad54d9e7516f172c1b4d909c47c30ee884
Matej Penciak <96667244+mpenciak@users.noreply.github.com>
2023-03-30 10:26:38 -0400

    Update lakefile.lean

--- lakefile.lean
+++ lakefile.lean
@@ -6,9 +6,9 @@ package LightData
 lean_lib LightData
 
 require YatimaStdLib from git
-  "https://github.com/lurk-lab/YatimaStdLib.lean" @ "10f2b444390a41ede90ca5c038c6ff972014d433"
+  "https://github.com/mpenciak/YatimaStdLib.lean" @ "10f2b444390a41ede90ca5c038c6ff972014d433"
 
 require LSpec from git
-  "https://github.com/yatima-inc/lspec/" @ "88f7d23e56a061d32c7173cea5befa4b2c248b41"
+  "https://github.com/mpenciak/lspec/" @ "88f7d23e56a061d32c7173cea5befa4b2c248b41"
 
 lean_exe Tests.LightData
